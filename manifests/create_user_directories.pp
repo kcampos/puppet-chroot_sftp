586dae7b648f1e90c82d5bda95607d2b5dd14a83
Kyle Campos <kyle.campos@gmail.com>
2014-12-23 12:05:06 -0700

    Add creat_user_directories feature

---
+++ manifests/create_user_directories.pp
@@ -0,0 +1,16 @@
+define chroot_sftp::create_user_directories(
+  $username = $name,
+  $directories,
+  ) {
+  include chroot_sftp::params
+
+  $basedir = "${chroot_sftp::params::chroot_basedir}/${username}"
+  $dirs    = prefix($directories, "${basedir}/")
+  file { $dirs:
+    ensure  => directory,
+    owner   => $username,
+    group   => $chroot_sftp::params::group_name,
+    mode    => "0755",
+    require => File[$basedir]
+  }
+}
---
+++ spec/defines/create_user_directories_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'chroot_sftp::create_user_directories' do
+  let(:dirs) { ["drop","pickup"] }
+  let(:username) { "user_pass" } # hiera fixture
+  let(:title) { username }
+  let(:params) { {:directories => dirs}}
+
+  it { should contain_file("/sftp/#{username}/drop").with({
+      'ensure' => 'directory',
+      'mode'   => '0755',
+      'owner'  => username,
+      'group'  => 'sftpusers'
+    })
+  }
+
+  it { should contain_file("/sftp/#{username}/pickup").with({
+      'ensure' => 'directory',
+      'mode'   => '0755',
+      'owner'  => username,
+      'group'  => 'sftpusers'
+    })
+  }
+end
