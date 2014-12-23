fb7b1b30bffbbcb1dca441af1fdaf810a42e6ddd
Kyle Campos <kyle.campos@gmail.com>
2014-12-23 15:06:28 -0700

    Add selinux support

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,4 +35,19 @@ class chroot_sftp($configure_ssh = false) inherits chroot_sftp::params {
       }
     }
   }
+
+  if $selinux_enforced {
+    exec { "enable_chroot_rw_access":
+      command => 'setsebool -P ssh_chroot_rw_homedirs on',
+      path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+      unless  => "getsebool ssh_chroot_rw_homedirs | awk '{ print \$3 }' | grep on",
+      notify  => Exec["restorecon-${chroot_basedir}"]
+    }
+
+    exec { "restorecon-${chroot_basedir}":
+      command     => "restorecon -R ${chroot_basedir}",
+      path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+      refreshonly => true,
+    }
+  }
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -14,5 +14,29 @@ describe 'chroot_sftp' do
     'options' => 'defaults',
     }).that_requires("File[/sftp]")
   }
+
+  context "with selinux enforced" do
+    let(:facts) { {:selinux_enforced => true} }
+
+    it { should contain_exec("enable_chroot_rw_access").with({
+      'command' => 'setsebool -P ssh_chroot_rw_homedirs on',
+      'path'    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+      'unless'  => "getsebool ssh_chroot_rw_homedirs | awk '{ print \$3 }' | grep on"
+      }).that_notifies("Exec[restorecon-/sftp]")
+    }
+
+    it { should contain_exec("restorecon-/sftp").with({
+      'command'     => "restorecon -R /sftp",
+      'path'        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+      'refreshonly' => true
+      })
+    }
+  end
+
+  context "with selinux disabled" do
+    let(:facts) { {:selinux_enforced => false} }
+
+    it { should have_exec_resource_count(0) }
+  end
   
 end
